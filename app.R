6bc017a0b7733793ee4f5e4eab1f1f1e7485e5ab
Lilith <LilithKramer@users.noreply.github.com>
2020-04-24 15:10:01 +0200

    ShinyPCModel v1.2
    - Added a folder that contains all possible output names, so these can be checked against the available columns. In this way the descriptive columns (time/runid) can separated from the columns containing model output values.
    - Made a switch to make working with R output and Excel output possible.
    - Switched to different way to select the variable columns. This way takes less room and works easier. (to be tested further).
    - Adjusted intro text
    - Built in some extra checks so the app does not give ugly errors in the app screen.

--- app.R
+++ app.R
@@ -3,8 +3,6 @@
 ## Author: L. Kramer
 ## Date:   16 April 2020
 
-## App cannot work with results that have differences in their selection of outputs!
-
 
 ##~~~~~~~~~~~~~~~~~~~~~
 ##==== Preparation ====
@@ -41,7 +39,8 @@ library(data.table)
 library(shiny)         
 library(shinydashboard)
 library(gridExtra)
-
+library(plotly)
+library(rlang)
 
 ##== functions ======
 
@@ -67,11 +66,18 @@ options(shiny.maxRequestSize = 30*1024^2) ## maximum upload size is now 30Mb, st
 #!# directories
 #dirInp <- "input/"
 ## dirInpPCModel <- "C:/Users/Lilith Kramer/Documents/PhD/Documenten/3.0. PhD inhoudelijk/3.6. PCLake/PCModel-master/Licence_agreement/I_accept/PCModel1350/PCModel/3.00/Models/PCLake+/6.13.16/Txt"
-#dirSet <- "settings/"
+dirHome <- "C:/Users/Lilith Kramer/Documents/PhD/Documenten/3.0. PhD inhoudelijk/3.8. R/ShinyPCModel/"
+dirSet <- "settings/"
 #dirOut <- "output/"
 
+fnSettings <- "output_names.csv"
 
- 
+## load file
+dtOutNames <- fread(paste(dirHome, dirSet, fnSettings, sep = ""))
+dtOutNames$Rname <- gsub("_", "", dtOutNames$Excelname)
+cOutNamesR <- unique(dtOutNames$Rname)
+cOutNamesExcel <- unique(dtOutNames$Excelname)
+
 ##==User Interface Start==================
 
 header  <- dashboardHeader(title = "Shiny PCModel")
@@ -91,11 +97,18 @@ body <- dashboardBody(
     #===start_tab==============
     tabItem(tabName = "start",
             fluidRow(
-              box(title = "Load data",
+              box(title = "Import data",
                   solidHeader = T,
                   status = "success",
                   collapsible = F,
-                  p("Maximum file size is 30MB. Only .txt files are accepted.", br(), "Multiple files can be added, but", strong(" DO NOT "), "add files that differ in their output columns!"),
+                  p("Multiple files can be imported at the same time. Each file should contain one run only.
+                    The files can be in the output format of either PCLake+ Excel or PCLake+ R. 
+                    Only .txt files are accepted", br(), 
+                    "If you choose the", strong(" Excel format"), ", the .txt files can miss headers. However, at least one file should contain the headers and", em("all files"), "should have the same output columns in the same order.
+                    It is also expected that the 'RunId' column is the first column in the .txt file that contains the headers.", br(),
+                    "If you choose the", strong(" R format "), ",", em(" all files"), " should contain headers. However, the order of the headers does not matter.", br(),
+                    "Maximum file size is 30MB.",),
+                  radioButtons("model_type", label = "Data source", choices = c("Excel", "R"), selected = "R"),
                   fileInput("input_data", NULL,
                             multiple = TRUE,
                             accept = c("text/txt"),
@@ -108,10 +121,8 @@ body <- dashboardBody(
                   status = "success",
                   collapsible = F,
                   uiOutput("checkbox_geomlines"),
-                  tags$div(align = 'left', 
-                           class = 'multicol',
-                           style = 'overflow-x: scroll',
-                           uiOutput("checkbox_facets")),
+                  p(""),
+                  uiOutput("checkbox_facets"),
                   div(style = 'overflow-y: scroll', uiOutput("plotgraph")),
                   width = 12
               )))))
@@ -143,20 +154,55 @@ server <- function(input, output, session) {
     return(input_list)
     })
   
+  
   input_data <- reactive({
 
-   read_datatables <- lapply(input_files(), fread_AddFilename)
-   names_all_cols <- colnames(read_datatables[[which(sapply(read_datatables, function(y) "RunId" %in% colnames(y)))]])
+    read_datatables <- lapply(input_files(), fread_AddFilename)
+    
+    if(input$model_type == "Excel"){
+      
+      validate(
+        need(length(which(sapply(read_datatables, function(y) "RunId" %in% colnames(y)[1]))) > 0,
+             "Please check the file format. You have selected Excel as the source of your text files. However, the RunId column name is either not the header of the first column or the RunId column name is missing in all input files.")
+        )
+      
+      names_all_cols <- colnames(read_datatables[[which(sapply(read_datatables, function(y) "RunId" %in% colnames(y)))]])
+      
+      make_datatable <- rbindlist(read_datatables, use.names = F)
+      if(colnames(make_datatable)[1] != "RunId"){colnames(make_datatable) <- names_all_cols}
+      if("-1" %in% colnames(make_datatable)){make_datatable[, which(colnames(make_datatable) == "-1"):= NULL]}
+      changeCols_numeric <- intersect(colnames(make_datatable), c(cOutNamesExcel, "Time", "time"))
+      make_datatable[,(changeCols_numeric):= lapply(.SD, as.numeric), .SDcols = changeCols_numeric]
+      changeCols_factor <- setdiff(colnames(make_datatable), changeCols_numeric)
+      make_datatable[,(changeCols_factor):= lapply(.SD, FUN = function(x) as.factor(as.character(x))), .SDcols = changeCols_factor]
+    } else {
+      
+      validate(
+        need(length(which(sapply(read_datatables, function(y) "V1" %in% colnames(y)))) == 0,
+             "You have selected R as the source of your text files. However, column headers are missing in at least one input file. Please check the file format.")
+      )
+      
+      if(length(read_datatables)==1){
+        make_datatable <- read_datatables[[1]]  
+      }
+      
+      if(length(read_datatables)>1){
 
-   make_datatable <- rbindlist(read_datatables, use.names = F)
-   if(colnames(make_datatable)[1] != "RunId"){colnames(make_datatable) <- names_all_cols}
-   make_datatable[, which(colnames(make_datatable) == "-1"):= NULL]
-   
-   changeCols_numeric <- grep("^_", colnames(make_datatable), value = T) ## first character = _
-   make_datatable[,(changeCols_numeric):= lapply(.SD, as.numeric), .SDcols = changeCols_numeric]
-   changeCols_factor <- c("RunId", "filename")
-   make_datatable[,(changeCols_factor):= lapply(.SD, FUN = function(x) as.factor(as.character(x))), .SDcols = changeCols_factor]
-   return(make_datatable)
+        make_datatable <- as.data.table(read_datatables %>% reduce(full_join))
+  
+        }
+      
+      if("-1" %in% colnames(make_datatable)){make_datatable[, which(colnames(make_datatable) == "-1"):= NULL]}
+      changeCols_numeric <- intersect(colnames(make_datatable), c(cOutNamesR, "Time", "time")) ## first character = _
+      make_datatable[,(changeCols_numeric):= lapply(.SD, as.numeric), .SDcols = changeCols_numeric]
+      changeCols_factor <- setdiff(colnames(make_datatable), changeCols_numeric)
+      make_datatable[,(changeCols_factor):= lapply(.SD, FUN = function(x) as.factor(as.character(x))), .SDcols = changeCols_factor]
+      
+      if("time" %in% colnames(make_datatable)){colnames(make_datatable)[which(colnames(make_datatable)=="time")] <- "Time"}
+      
+    }
+
+     return(make_datatable)
   
  })
  
@@ -167,52 +213,54 @@ server <- function(input, output, session) {
     })
  
   molten_data <- reactive({
-    totally_molten <- melt(input_data(), id.vars = c("RunId","Time", "filename"))
-    setkey(totally_molten, filename, variable)
+    if(input$model_type == "Excel"){toMelt <- setdiff(colnames(input_data()), intersect(colnames(input_data()), cOutNamesExcel))}
+    if(input$model_type == "R"){toMelt <- setdiff(colnames(input_data()), intersect(colnames(input_data()), cOutNamesR))}
+    #toMelt <- grep("RunId|filename|time|Time", colnames(input_data()), value = T)
+    totally_molten <- melt(input_data(), id.vars = toMelt)
     return(totally_molten)
   })
 
-  output$checkbox_facets <- renderUI({
-    var_options <- unique(molten_data()$variable)
-    checkboxGroupInput("variable_options", "Choose variables:", var_options, inline = FALSE)
-  })
-  
   output$checkbox_geomlines <- renderUI({
     file_options <- unique(molten_data()$filename)
-    checkboxGroupInput("run_options", "Choose run:", file_options, selected = 1, inline = TRUE)
+    checkboxGroupInput("run_options", "Choose run:", file_options, selected = file_options[1], inline = TRUE)
+  })
+  
+  output$checkbox_facets <- renderUI({
+    if(input$model_type == "Excel"){keepIn <- intersect(colnames(input_data()), cOutNamesExcel)}
+    if(input$model_type == "R"){keepIn <- intersect(colnames(input_data()), cOutNamesR)}
+    
+    input_data_adj <- input_data()[, ..keepIn]
+    varSelectInput("variable_options", "Choose variables:", input_data_adj, multiple = TRUE)
   })
   
   
   # this function defines a height of your plot
   plot_height <- function() {
     req(input$variable_options)
-    amount_of_plots <- length(input$variable_options) * 200 
+    amount_of_plots <- max(1, length(input$variable_options)) * 200 
     return(amount_of_plots)
   }
   
   # this function defines your plot, which depends on input$dimension1In
   output$contents <- renderPlot({
-    req(input$variable_options, input$run_options)
     
-    # subset_data <- molten_data()[.(input$run_options, input$variable_options),]
+    req(length(input$variable_options)>0, input$run_options)
+    
     subset_data <- molten_data()[(filename %in% input$run_options) & (variable %in% input$variable_options),]
-    
-    
     
     plot <- ggplot(subset_data, aes(x = Time, y = value, color = filename)) +
       geom_line() +
       facet_wrap("variable", ncol = 1, scales = "free_y") +
       theme_bw()
     
-  return(plot)
+    return(plot)
   })
-  
+
   # wrap plotOutput in renderUI
   output$plotgraph <- renderUI({
     plotOutput("contents", height = plot_height(), width = "100%")
   })
   
-
   
 }
 
